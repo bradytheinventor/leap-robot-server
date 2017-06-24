e2e044124735fd0f554997352c33c29cfa9c1d70
bradytheinventor <bcra1234@gmail.com>
2017-06-24 15:02:47 -0500

    Added initial robot code
    Added initial robot code outline.

---
+++ src/org/usfirst/frc/team2823/robot/Robot.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team2823.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+	
+	@Override
+	public void robotInit() {
+		
+	}
+	
+	@Override
+	public void autonomousInit() {
+		
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+	}
+	
+	@Override
+	public void testPeriodic() {
+	}
+}
+
